31a5a9a64d0d89ccf908ef0f5aa03c98ab247cd7
Harshavardhan Reddy Gadila <harshavardhanreddy.gadila@sjsu.edu>
2025-05-10 14:42:13 -0700

    Update PasswordRequirements.js

--- frontend/src/components/PasswordRequirements.js
+++ frontend/src/components/PasswordRequirements.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+// Component to visually display which password requirements are met
 const PasswordRequirements = ({ password = '' }) => {
-  // Default to empty string if password is undefined
-  
+  // Default password to empty string to avoid errors if undefined
+
+  // Inline styles for the container and list items
   const styles = {
     container: {
       marginTop: '10px',
@@ -17,15 +19,15 @@ const PasswordRequirements = ({ password = '' }) => {
     },
     item: {
       margin: '3px 0',
-      color: '#888'
+      color: '#888' // Gray for unmet criteria
     },
     metItem: {
       margin: '3px 0',
-      color: 'green'
+      color: 'green' // Green for met criteria
     }
   };
 
-  // Define requirements checks
+  // Boolean flags for each requirement
   const hasMinLength = password.length >= 8;
   const hasUppercase = /[A-Z]/.test(password);
   const hasLowercase = /[a-z]/.test(password);
@@ -36,6 +38,7 @@ const PasswordRequirements = ({ password = '' }) => {
     <div style={styles.container}>
       <p>Password must contain:</p>
       <ul style={styles.list}>
+        {/* Conditionally style each requirement based on whether it is met */}
         <li style={hasMinLength ? styles.metItem : styles.item}>
           At least 8 characters
         </li>
@@ -49,7 +52,7 @@ const PasswordRequirements = ({ password = '' }) => {
           At least one number
         </li>
         <li style={hasSpecial ? styles.metItem : styles.item}>
-          At least one special character (!@#$%^&*()-_=+[]{}|;:,.&lt;&gt;/?)
+          At least one special character (!@#$%^&*()-_=+[]{{}}|;:,.<>/?)
         </li>
       </ul>
     </div>
